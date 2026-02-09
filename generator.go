8514f41bfb6d37887f7d722a94785cf1840e535c
skrik2 <skrik2@outlook.com>
2026-02-09 23:58:25 +0800

    init

---
+++ generator.go
@@ -0,0 +1,99 @@
+package uuid
+
+import (
+	"crypto/rand"
+	"io"
+	"time"
+)
+
+type Generator interface {
+	NewV4() (UUID, error)
+	// NewV7() UUID
+	NewV7Lazy() (UUID, error)
+}
+
+func (u *UUID) setVersion(v byte) {
+	u[6] = (u[6] & 0x0F) | (v << 4)
+}
+
+// SetVariant sets the variant bits.
+func (u *UUID) setVariant(v byte) {
+	switch v {
+	case VariantNCS:
+		u[8] = (u[8]&(0xff>>1) | (0x00 << 7))
+	case VariantRFC9562:
+		u[8] = (u[8]&(0xff>>2) | (0x02 << 6))
+	case VariantMicrosoft:
+		u[8] = (u[8]&(0xff>>3) | (0x06 << 5))
+	case VariantFuture:
+		fallthrough
+	default:
+		u[8] = (u[8]&(0xff>>3) | (0x07 << 5))
+	}
+}
+
+// Difference in 100-nanosecond intervals between
+// UUID epoch (October 15, 1582) and Unix epoch (January 1, 1970).
+const epochStart = 122192928000000000
+
+type gen struct {
+	rand io.Reader
+}
+
+var defaultGen = newDefaultGen()
+
+var _ Generator = (*gen)(nil)
+
+// type GenOption func(*defaultGen)
+
+func newDefaultGen() *gen {
+	return &gen{
+		rand: rand.Reader,
+	}
+}
+
+func (g *gen) NewV4() (UUID, error) {
+	u := UUID{}
+	if _, err := io.ReadFull(g.rand, u[:]); err != nil {
+		return NilUUID, err
+	}
+	u.setVersion(V4)
+	u.setVariant(VariantRFC9562)
+	return u, nil
+}
+
+func (g *gen) NewV7Lazy() (UUID, error) {
+	// https://datatracker.ietf.org/doc/html/rfc9562#name-uuid-version-7
+	//
+	// UUIDv7 {
+	//     unix_ts_ms(0..47),
+	//     version(48..51),
+	//     rand_a(12),
+	//     variant(64..65),
+	//     rand_b(66..127)
+	// }
+	u := UUID{}
+
+	// UUIDv7 uses a 48-bit Unix timestamp in milliseconds.
+	ms := uint64(time.Now().UnixMilli())
+	// Bytes 0-5: 48-bit big-endian Unix millisecond timestamp
+	u[0] = byte(ms >> 40)
+	u[1] = byte(ms >> 32)
+	u[2] = byte(ms >> 24)
+	u[3] = byte(ms >> 16)
+	u[4] = byte(ms >> 8)
+	u[5] = byte(ms)
+
+	//cryptographically random tail
+	if _, err := io.ReadFull(g.rand, u[6:16]); err != nil {
+		return NilUUID, err
+	}
+
+	//override first 4bits of u[6].
+	u.setVersion(V7)
+
+	//override first 2 bits of byte[8] for the variant
+	u.setVariant(VariantRFC9562)
+
+	return u, nil
+}
---
+++ uuid.go
@@ -0,0 +1,205 @@
+// https://datatracker.ietf.org/doc/html/rfc9562
+
+package uuid
+
+import (
+	"database/sql/driver"
+	"fmt"
+	"time"
+)
+
+type UUID [16]byte
+
+// UUID versions.
+const (
+	_  byte = iota
+	V1      // Version 1 (date-time and MAC address) [no implement]
+	_       // Version 2 (date-time and MAC address, DCE security version) [removed]
+	V3      // Version 3 (namespace name-based) [no implement]
+	V4      // Version 4 (random)
+	V5      // Version 5 (namespace name-based) [no implement]
+	V6      // Version 6 (k-sortable timestamp and random data, field-compatible with v1) [no implement]
+	V7      // Version 7 (k-sortable timestamp and random data)
+	_       // Version 8 (k-sortable timestamp, meant for custom implementations) [not implemented]
+)
+
+// NilUUID is the nil UUID, as specified in RFC-9562, that has all 128 bits set to zero.
+var NilUUID = UUID{}
+
+// Max is the maximum UUID, as specified in RFC-9562, that has all 128 bits
+// set to one.
+var Max = UUID{
+	0xFF,
+	0xFF,
+	0xFF,
+	0xFF,
+	0xFF,
+	0xFF,
+	0xFF,
+	0xFF,
+	0xFF,
+	0xFF,
+	0xFF,
+	0xFF,
+	0xFF,
+	0xFF,
+	0xFF,
+	0xFF,
+}
+
+// UUID layout variants.
+const (
+	VariantNCS byte = iota
+	VariantRFC9562
+	VariantMicrosoft
+	VariantFuture
+)
+
+func NewV4() (UUID, error) {
+	return defaultGen.NewV4()
+}
+
+func NewV7() (UUID, error)
+
+// NewV7Lazy generates a V7 UUID with a 48-bit Unix millisecond timestamp
+// and a fully random tail. This version does not guarantee monotonicity
+// within the same millisecond.
+//
+// UUIDv7 layout (bit positions):
+//
+//	0..47   unix_ts_ms
+//	48..51  version
+//	52..63  rand_a
+//	64..65  variant
+//	66..127 rand_b
+func NewV7Lazy() (UUID, error) {
+	return defaultGen.NewV7Lazy()
+}
+
+// func NewV4Rand(rand io.Reader) UUID
+// func NewV7AtTime(t time.Time) UUID
+// func NewV7AtTimeRand(t time.Time, rand io.Reader) UUID
+
+// Version returns the algorithm version used to generate the UUID.
+func (u UUID) Version() byte {
+	return u[6] >> 4
+}
+
+// Must is a helper that wraps a call to a function returning (UUID, error) and panics
+// if the error is non-nil. It is intended for use in variable initializations such as
+//
+//	var packageUUID = uuid.MustUUID(uuid.FromString("123e4567-e89b-12d3-a456-426655440000"))
+func MustUUID(u UUID, err error) UUID {
+	if err != nil {
+		panic(err)
+	}
+	return u
+}
+func FromBytes(input []byte) (UUID, error)
+func FromBytesOrNil(input []byte) UUID
+
+func FromString(input string) (UUID, error)
+
+// Parse parses the UUID stored in the string text. Parsing
+// and supported formats are the same as UnmarshalText.
+func (u *UUID) Parse(s string) error
+
+func FromStringOrNil(input string) UUID
+
+// encodeCanonical encodes the canonical RFC-9562 form of UUID u into the
+// first 36 bytes dst.
+func encodeCanonical(dst []byte, u UUID) {
+	const hextable = "0123456789abcdef"
+	dst[8] = '-'
+	dst[13] = '-'
+	dst[18] = '-'
+	dst[23] = '-'
+	for i, x := range [16]byte{
+		0, 2, 4, 6,
+		9, 11,
+		14, 16,
+		19, 21,
+		24, 26, 28, 30, 32, 34,
+	} {
+		c := u[i]
+		dst[x] = hextable[c>>4]
+		dst[x+1] = hextable[c&0x0f]
+	}
+}
+
+// String returns a canonical RFC-9562 string representation of the
+// UUID: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx.
+func (u UUID) String() string {
+	var buf [36]byte
+	encodeCanonical(buf[:], u)
+	return string(buf[:])
+}
+
+// Bytes returns a newly allocated byte slice containing the UUID.
+// Modifying the returned slice will NOT affect the original UUID.
+func (u UUID) Bytes() []byte {
+	b := make([]byte, 16)
+	copy(b, u[:])
+	return b
+}
+
+// AsSlice returns a byte slice referencing the underlying UUID array.
+// Modifying the returned slice WILL modify the UUID itself.
+func (u *UUID) AsSlice() []byte {
+	return u[:]
+}
+
+// Format implements fmt.Formatter for UUID values.
+// The behavior is as follows: The 'x' and 'X' verbs output only the hex digits of the UUID,
+// using a-f for 'x' and A-F for 'X'. The 'v', '+v', 's' and 'q' verbs return the canonical RFC-9562 string
+// representation. The 'S' verb returns the RFC-9562 format, but with capital hex digits. The '#v' verb returns
+// the "Go syntax" representation, which is a 16 byte array initializer. All other verbs not handled directly by the
+// fmt package (like '%p') are unsupported and will return "%!verb(uuid.UUID=value)" as recommended by the fmt package.
+func (u UUID) Format(f fmt.State, c rune)
+
+// IsNilUUID returns if the UUID is equal to the nil UUID
+func (u UUID) IsNilUUID() bool {
+	return u == NilUUID
+}
+
+func (u UUID) Equal(another UUID) bool
+
+func (u UUID) Compare(v UUID) int
+
+func (u UUID) Time() time.Time // only V7
+
+// MarshalBinary implements the encoding.BinaryMarshaler interface.
+func (u UUID) MarshalBinary() ([]byte, error)
+
+// UnmarshalBinary implements the encoding.BinaryUnmarshaler interface.
+// It will return an error if the slice isn't 16 bytes long.
+func (u *UUID) UnmarshalBinary(data []byte) error
+
+// MarshalText implements the encoding.TextMarshaler interface.
+// The encoding is the same as returned by the String() method.
+func (u UUID) MarshalText() ([]byte, error)
+
+func (u *UUID) UnmarshalText(b []byte) error
+
+// Value implements the driver.Valuer interface.
+func (u UUID) Value() (driver.Value, error)
+
+// Scan implements the sql.Scanner interface. A 16-byte slice will be handled by UnmarshalBinary,
+// while a longer byte slice or a string will be handled by UnmarshalText.
+func (u *UUID) Scan(src interface{}) error
+
+// Variant returns the UUID layout variant.
+func (u UUID) Variant() byte {
+	switch {
+	case (u[8] >> 7) == 0x00:
+		return VariantNCS
+	case (u[8] >> 6) == 0x02:
+		return VariantRFC9562
+	case (u[8] >> 5) == 0x06:
+		return VariantMicrosoft
+	case (u[8] >> 5) == 0x07:
+		fallthrough
+	default:
+		return VariantFuture
+	}
+}
